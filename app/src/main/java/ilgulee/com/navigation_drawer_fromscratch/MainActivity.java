2105de9b17cd2722c7804192fbb3a24f81c32ba0
Ilgu Lee <iglee2000ca@gmail.com>
2018-06-03 14:13:14 -0400

    Initial commit

---
+++ app/src/main/java/ilgulee/com/navigation_drawer_fromscratch/MainActivity.java
@@ -0,0 +1,18 @@
+package ilgulee.com.navigation_drawer_fromscratch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+}
